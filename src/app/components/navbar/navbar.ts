25ef7a3d4b7e1a36f08e6efb17612ff503c335f6
Paul Gonzalez-Becerra <pgonzbecer@gmail.com>
2017-05-01 12:24:53 -0700

    That sick navbar thing (disabled)

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -8,6 +8,21 @@ export class NavbarComponent implements OnInit{
 
   constructor(){}
 
-  ngOnInit() {}
+  ngOnInit() {
+    
+    /*
+    // This is to change the icon into other colors
+    // Variables
+    var brand=  document.getElementById("brandman");
+    var deg=  Number(brand.style.filter.match(/[0-9]+/)[0]);
+    
+    setInterval(function() {
+      deg+= 1;
+      deg%= 360;
+      brand.style.filter= "hue-rotate("+deg+"deg)";
+      var seq= deg/360;
+    }, 25);
+    */
+  }
 
 }
