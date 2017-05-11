c523d382a3040aca46e400d15636ef97ba761c02
Monk <sabintheworld@gmail.com>
2017-05-11 09:52:37 -0700

    Flex-Styling Stuff
    Homepage and Profile page styles using flex

--- src/app/pages/profile/profile-banner/profile-banner.ts
+++ src/app/pages/profile/profile-banner/profile-banner.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit 	} from '@angular/core';
+declare var $: any;
 @Component({
   selector: 'profile-banner',
   templateUrl: './profile-banner.html',
@@ -9,7 +10,16 @@ export class ProfileBannerComponent implements OnInit{
 	constructor(){}
 
 	ngOnInit(){
-		
+		$(function(){
+		  var $ppc = $('.progress-pie-chart'),
+		    percent = parseInt($ppc.data('percent')),
+		    deg = 360*percent/100;
+		  if (percent > 50) {
+		    $ppc.addClass('gt-50');
+		  }
+		  $('.ppc-progress-fill').css('transform','rotate('+ deg +'deg)');
+		  
+		});
 	}
 	
 }
