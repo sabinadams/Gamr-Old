e684c643a333bf3dde6924e7eee8c39a0ecd50e5
Monk <sabintheworld@gmail.com>
2017-05-13 03:34:34 -0700

    Revert "graphical glitch"
    This reverts commit 9748a4826b2953b01f37d606e550b556276449fe.

--- src/app/pipes/exp-calc-pipe.ts
+++ src/app/pipes/exp-calc-pipe.ts
@@ -3,28 +3,19 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({name: 'calcExp'})
 export class ExpCalcPipe implements PipeTransform {
-	dummy_xp=	125;
+	dummy_xp=	370;
 	
   transform(exp: number, type: string): string	{
 		if(exp=== null || exp=== undefined)
 			exp=	this.dummy_xp;
 		switch(type)	{
 			case "curr":	return exp.toString();
-			case "next":
-				if(exp=== this.getNextXP(this.getLevel(exp)))
-					return this.getNextXP(this.getLevel(exp)+1).toLocaleString();
-				return this.getNextXP(this.getLevel(exp)).toLocaleString();
-			case "getLevel":	
-				if(exp=== this.getNextXP(this.getLevel(exp)))
-					return (this.getLevel(exp)+1).toLocaleString();
-				return this.getLevel(exp).toLocaleString();
+			case "next":	return this.getNextXP(this.getLevel(exp)).toLocaleString();
+			case "getLevel":	return this.getLevel(exp).toLocaleString();
 			case "getPerc":
 				var	past=	this.getNextXP(this.getLevel(exp)-1);
-				var	perc=	Math.trunc(100*((exp-past)/(this.getNextXP(this.getLevel(exp))-past)));
 				
-				if(perc=== 100)
-					return "0";
-				return perc.toString();
+				return Math.trunc(100*((exp-past)/(this.getNextXP(this.getLevel(exp))-past))).toLocaleString();
 		}
   }
   
