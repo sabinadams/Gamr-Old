768c2c1194309dc1067c26c56b2035bbb69fba10
Monk <sabintheworld@gmail.com>
2017-05-13 03:48:56 -0700

    Revert "  test"
    This reverts commit 4fd4317234e0e5c12b991f360c19f8b2b7d82c7a.

--- src/app/components/profile-side-panel/profile-side-panel.ts
+++ src/app/components/profile-side-panel/profile-side-panel.ts
@@ -1,5 +1,5 @@
-import { Component, Input } from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
+declare var $: any;
 @Component({
   selector: 'profile-side-panel',
   templateUrl: './profile-side-panel.html',
@@ -9,4 +9,16 @@ export class ProfileSidePanelComponent {
 
 	user = JSON.parse( localStorage.getItem( 'user' ) );
 
+	ngOnInit(){
+		$(function(){
+		  var $ppc = $('.progress-pie-chart');
+		  var percent = parseInt($ppc.data('percent'));
+		  var deg = 360*percent/100;
+		  if (percent > 50) {
+		    $ppc.addClass('gt-50');
+		  }
+		  $('.ppc-progress-fill').css('transform','rotate('+ deg +'deg)');
+		  
+		});
+	}
 }
