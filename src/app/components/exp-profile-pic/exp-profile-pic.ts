805eda5da7d23492755f8bf15b73984a983c7faa
Monk <sabintheworld@gmail.com>
2017-05-24 16:32:45 -0700

    Cleaned up Profile EXP Thingy

--- src/app/components/exp-profile-pic/exp-profile-pic.ts
+++ src/app/components/exp-profile-pic/exp-profile-pic.ts
@@ -1,19 +1,28 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { ExpCalcPipe } from '../../pipes/exp-calc-pipe';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'exp-profile-pic',
-  templateUrl: './exp-profile-pic.html',
-  styleUrls: ['./exp-profile-pic.scss'],
+  inputs: ['user'],
+  styles: [`
+  	.round-progress {
+  	     background-size: cover !important;
+  	     border-radius:50% !important;
+  	     margin-bottom:10px;
+  	 }
+  `],
+  template: `
+	<round-progress class="round-progress"
+	    [ngStyle]="{background: 'url(' + user.profile_pic + ')'}"
+	    [current]="user.exp_count" [max]="user.exp_count | calcExp: 'next' "
+	    [color]="'#956bb9'" [background]="'rgba(0,0,0,0.5)'"
+	    [radius]="65" [stroke]="5"
+	    [semicircle]="false" [rounded]="false" [clockwise]="true"
+	    [duration]="800" [animation]="'easeInOutQuart'" [animationDelay]="2">
+	</round-progress>
+	<p counto [step]="30" [duration]="1" [countTo]="user.exp_count" [countFrom]="0" (countoChange)="counto = $event">
+		{{ counto | calcExp: "curr" }} / {{ user.exp_count | calcExp: "next" }} exp
+	</p>
+	<h4>@{{user.tag}} <small>Lvl. {{ user.exp_count | calcExp: "getLevel" }}</small></h4>
+  `
 })
-export class ExpProfilePic implements OnInit{
-
-	@Input() user: any = 0;
-	constructor(){
-	}
-
-	ngOnInit(){
-		let expCalcPipe = new ExpCalcPipe();
-		this.user.next_level_xp = expCalcPipe.transform(this.user.exp_count, 'next');
-	}
-}
+export class ExpProfilePic {}
