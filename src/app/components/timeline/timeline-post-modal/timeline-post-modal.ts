13ece900cfeeabdc2cfea466c4643adc31ec031f
Monk <sabintheworld@gmail.com>
2017-06-05 14:04:06 -0700

    Preparing Comment Forms

--- src/app/components/timeline/timeline-post-modal/timeline-post-modal.ts
+++ src/app/components/timeline/timeline-post-modal/timeline-post-modal.ts
@@ -1,5 +1,6 @@
 import { Component, Input, Output, EventEmitter, OnInit, ViewEncapsulation } from '@angular/core';
 import { SocialService } from '../../../services/social-service';
+import { ImageService } from '../../../services/image-service';
 import { Router } from '@angular/router';
 import { Lightbox, LightboxConfig } from 'angular2-lightbox';
 
@@ -12,8 +13,15 @@ export class TimelinePostModal implements OnInit{
   @Input() post: any;
   @Output() destroyPost = new EventEmitter();
   user = JSON.parse( localStorage.getItem('user') );
+  new_post = { text: '', images: [], video: '' };
   regex: any;
-  constructor( private _lightboxConfig: LightboxConfig, private _lightbox: Lightbox, private _socialService: SocialService ) {}
+  uploading = false;
+  constructor( 
+    private _imageService: ImageService,
+    private _lightboxConfig: LightboxConfig,
+    private _lightbox: Lightbox,
+    private _socialService: SocialService
+  ) {}
 
   ngOnInit() {
     this._lightboxConfig.wrapAround = true;
@@ -44,4 +52,29 @@ export class TimelinePostModal implements OnInit{
     this._lightboxConfig.positionFromTop = event.pageY  - event.y + event.y / 4;
     this._lightbox.open(this.post.images, index);
   }
+
+  // Sends the image through a function whenever the file input is used
+  imageHandler($event): void {
+    if (this.new_post.images.length < 7 && $event.target.files[0] !== undefined) {
+      this.saveImage($event.target);
+    }
+  }
+
+  // Converts file to base64
+  // Try to add this to the image service. Promises will probably need to be applied
+  saveImage(inputValue: any): void {
+    this.uploading = true;
+    const file: File = inputValue.files[0];
+    const myReader: FileReader = new FileReader();
+    myReader.readAsDataURL(file);
+    myReader.onloadend = (e) => {
+      const newimage = myReader.result;
+      this._imageService.uploadImage(newimage).subscribe(res => {
+        this.new_post.images.push(res.data.link);
+        this.uploading = false;
+      });
+    };
+  }
+  removeImage(i) { this.new_post.images.splice(i, 1); }
+
 }
