0921d20d38a87eae5c0ae395b84f9772f6fa53a9
Paul Gonzalez-Becerra <pgonzbecer@gmail.com>
2017-06-29 21:52:27 -0700

    Hope will let you soar...
    ...like a car. BOOM! Nailed it!

--- src/app/services/cheat-codes.ts
+++ src/app/services/cheat-codes.ts
@@ -5,8 +5,7 @@ export class CheatCodesService {
 	// Variables
 	cheatCodes=	JSON.parse(localStorage.getItem("cheatCodes")) || [
 		["ArrowUp", "ArrowUp", "ArrowDown", "ArrowDown", "ArrowLeft", "ArrowRight", "ArrowLeft", "ArrowRight", "b", "a"],
-		["ArrowLeft", "ArrowRight", "ArrowDown", "a", "a", "b"],
-		["f", "e", "g", "1", "1", "3"]
+		["[", "]", "ArrowDown", "l", "2", "ArrowUp", "l", "1", "o", "ArrowUp", "x", "ArrowLeft"]
 	];
 	currCode=	JSON.parse(localStorage.getItem("currCode")) || [];
 	
@@ -52,14 +51,47 @@ export class CheatCodesService {
 	performAction(index:Number)	{
 		switch(index)	{
 			case 0:	{
-				// Hackerman
-				console.log("flerp");
+				// Variables
+				let x= document.getElementsByTagName("*");
+				
+				for(let i= 0; i< x.length; i++)	{
+					x[i].remove();
+				}
+				
+				let	b=	document.createElement("body");
+				let	q=	[];
+				
+				for(let i= 0; i< 4; i++)	{
+					q[i]=	document.createElement("img");
+					q[i].src=	"assets/punished_kojima.gif";
+					b.appendChild(q[i]);
+				}
+				
+				document.appendChild(b);
 			}break;
 			case 1:	{
-				console.log("ASDASDA");
-			}break;
-			case 2:	{
-				console.log("ASDASDFADSFWQEGWRG@#@$EVER E T$%#T@");
+				// Variables
+				let	temp=	document.createElement("div");
+				let	cvs=	document.createElement("canvas");
+				let	scripts=	[
+					document.createElement("script"),
+					document.createElement("script"),
+					document.createElement("script")
+				];
+				
+				temp.id=	"gx";
+				cvs.id=	"cvs";
+				scripts[0].src=	"assets/csmuga/csmuga.js";
+				scripts[1].src=	"assets/timer.min.js";
+				scripts[2].innerHTML=	"setTimeout(function(){carshmup.runGame();}, 500);";
+				
+				temp.appendChild(cvs);
+				temp.appendChild(scripts[0]);
+				temp.appendChild(scripts[1]);
+				temp.appendChild(scripts[2]);
+				
+				document.body.appendChild(temp);
+				console.log("derp");
 			}break;
 		}
 	}
