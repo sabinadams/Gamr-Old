97ee52bc9bebb771d35ab570b7051d7e7b4ed289
Monk <sabintheworld@gmail.com>
2017-08-09 15:15:45 -0700

    Set up Environment Swap
    Environment Variables are stored in the BaseService now

---
+++ src/app/models/User.ts
@@ -0,0 +1,6 @@
+// export interface IUser {
+//    display_name: string,
+//    tag: string,
+//    ID: number,
+//    profile_pic: string
+// }
--- src/app/services/base-service.ts
+++ src/app/services/base-service.ts
@@ -1,12 +1,15 @@
 import { Injectable, isDevMode } from '@angular/core';
+import { environment } from '../../environments/environment';
+import * as models from '../models/index';
+
 @Injectable()
 export class BaseService {
-    // Replace this with a local instance of the server
-    public baseURL = isDevMode() ? 'http://api.gamr.co' : 'http://api.gamr.co';
+    
+    public env: models.IEnv = environment;
 
     constructor(){}
 
-    getUser() {
+    public getUser() {
         return JSON.parse(localStorage.getItem('user'));
     }
 
